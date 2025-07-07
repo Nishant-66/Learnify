e2a2d5ce27632099939b0d779218e8f30bd031c1
jaitly8x <jaitlyshantanu@gmail.com>
2025-07-08 02:58:43 +0530

    Login Logic added

---
+++ backend/controllers/auth.js
@@ -0,0 +1,31 @@
+const User=require('../models/user');
+const bcrypt=require('bcrypt');
+ const signup=async(req,res)=>{
+    try{
+        const {firstName,lastName,emailId,password}=req.body;
+        const hashedpassword=await bcrypt.hash(password,10);
+
+        const user=new User({
+            firstName,
+            lastName,
+            emailId,
+            password:hashedpassword,
+        })
+         
+        const savedUser=await user.save();
+        const token=savedUser.getJwt();
+        res.cookie("token",token, {
+            expires: new Date(Date.now() + 8 * 3600000),
+        });
+        res.json({
+            message:"User Added Successfully",
+            data:savedUser
+        })
+
+
+    }
+    catch(err){
+       res.status(400).send("ERROR while signup: " + err.message);
+    }
+
+}
