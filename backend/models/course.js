59c5d8fba1f4a4d3b8c12e58e39eafe127a71e5e
Nishant-66 <kumarnishant26346@gmail.com>
2025-07-08 19:06:33 +0530

    Course model created

---
+++ backend/models/course.js
@@ -0,0 +1,41 @@
+const mongoose=require('mongoose');
+const validator=require('validator');
+const courseSchema= new mongoose.Schema({
+    title:{
+        type:String,
+        required:true,
+        validate(value){
+            if(!validator.isLength(value,{max:100})){
+                throw new Error("Title must be at most 100 character long ")
+
+            }
+        }
+
+    },
+    description:{
+        type:String,
+        required:true,
+        validate(value){
+            const wordCount = value.trim().split(/\s+/).length;
+            if (wordCount > 100) {
+            throw new Error('Description must be at most 100 words.');
+            }
+        }
+    },
+    thumbnail:{
+        type:String,
+        required:true
+    },
+
+    price:{
+        type:Number,
+        required:true
+       
+    }
+
+
+},{
+    timestamps:true
+});
+
+module.exports=mongoose.model('Course',courseSchema);
